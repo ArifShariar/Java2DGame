e5279a16b942cf4932d48b4a1e34d238be77e2e9
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2020-03-21 15:08:42 +0600

    UP to tutorial 1: Added character sizing and alternate movement method

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -4,11 +4,45 @@ import java.awt.*;
 
 public abstract class Entity {
     protected float x, y;
-    public  Entity(float x, float y){
+    protected int width, height;
+    public  Entity(float x, float y, int width, int height){
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
     }
     public abstract void update();
     public abstract void render(Graphics g);
 
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -8,29 +8,35 @@ import java.awt.*;
 public class Player extends Creature{
     private Game game;
     public Player(Game game, float x, float y) {
-        super(x, y);
+        super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
         this.game = game;
     }
 
     @Override
     public void update() {
+        getInput();
+        move();
+    }
+    private void getInput(){
+        xMove = 0;
+        yMove = 0;
+
         if (game.getKeyManager().up){
-            y -= 3;
+            yMove = -speed;
         }
         if (game.getKeyManager().down){
-            y += 3;
+            yMove = +speed;
         }
         if (game.getKeyManager().left){
-            x -= 3;
+            xMove = -speed;
         }
         if (game.getKeyManager().right){
-            x += 3;
+            xMove = +speed;
         }
-
     }
 
     @Override
     public void render(Graphics g) {
-        g.drawImage(Assets.player,(int)x,(int)y,null);
+        g.drawImage(Assets.player,(int)x,(int)y,width,height,null);
     }
 }
