4eba9fc1beec9e16da3c2faa959adad1271da927
ArifShariar <47409421+ArifShariar@users.noreply.github.com>
2020-03-24 01:29:45 +0600

    Up to Tutorial 25: Implemented Character Animation

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,12 +1,19 @@
 package entity;
 
+import graphics.Animation;
 import graphics.Assets;
 import tileGame.Game;
 import tileGame.Handler;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Player extends Creature{
+    // Animation
+    private Animation animationDown;
+    private Animation animationUp;
+    private Animation animationLeft;
+    private Animation animationRight;
 
     public Player(Handler handler, float x, float y) {
         super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
@@ -14,10 +21,20 @@ public class Player extends Creature{
         bounds.y = 32;
         bounds.width = 30;
         bounds.height = 12; // change according to player size
+
+        animationDown = new Animation(166,Assets.player_down);
+        animationUp = new Animation(166,Assets.player_up);
+        animationLeft = new Animation(166,Assets.player_left);
+        animationRight = new Animation(166,Assets.player_right);
     }
 
     @Override
     public void update() {
+        animationDown.update();
+        animationUp.update();
+        animationRight.update();
+        animationLeft.update();
+
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
@@ -43,12 +60,28 @@ public class Player extends Creature{
     @Override
     public void render(Graphics g) {
 
-        g.drawImage(Assets.player,(int)(x-handler.getGameCamera().getxOffset()),
+        g.drawImage(getCurrentAnimationFrame(),(int)(x-handler.getGameCamera().getxOffset()),
                 (int)(y-handler.getGameCamera().getyOffset()),
                 width,height,null);
        // g.setColor(Color.RED);
         //g.fillRect((int)(x + bounds.x-handler.getGameCamera().getxOffset()),
           //      (int)(y + bounds.y-handler.getGameCamera().getyOffset()),
             //    bounds.width, bounds.height);
+    }
+
+    private BufferedImage getCurrentAnimationFrame(){
+        if(xMove<0){
+            return animationLeft.getCurrentFrame();
+        }
+        else if (xMove>0){
+            return animationRight.getCurrentFrame();
+        }
+        else if(yMove<0){
+            return animationUp.getCurrentFrame();
+        }
+        else {
+            return animationDown.getCurrentFrame();
+        }
+
     }
 }
--- src/graphics/Assets.java
+++ src/graphics/Assets.java
@@ -4,17 +4,57 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
     private static final int width = 32, height = 32;
-    public static BufferedImage dirt, grass, water, tree,player,stone,lava, brick;
+    private static final int playerWidth = 584, playerHeight = 584;
+    public static BufferedImage dirt, grass, water, tree,stone,lava, brick;
+    public static BufferedImage[] player_up;
+    public static BufferedImage[] player_down;
+    public static BufferedImage[] player_right;
+    public static BufferedImage[] player_left;
     public static void init(){
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/Sheet.png"));
-         dirt = sheet.crop(0,0,width,height);
-         grass = sheet.crop(width, 0, width, height);
-         water = sheet.crop(2*width, 0, width, height);
-         tree = sheet.crop(3*width,0, width, height);
-         player = sheet.crop(0, height, width, height);
-         stone =sheet.crop(width, height,width,height);
-         lava = sheet.crop(2*width,height,width,height);
-         brick = sheet.crop(3*width, height, width, height);
+        SpriteSheet sheet2 = new SpriteSheet(ImageLoader.loadImage("/textures/Animation1.png"));
+
+        player_right = new BufferedImage[6];
+        player_right[0] = sheet2.crop(0,0,width, height);
+        player_right[1] = sheet2.crop(width,0,width, height);
+        player_right[2] = sheet2.crop(width*2,0,width, height);
+        player_right[3] = sheet2.crop(width*3,0,width, height);
+        player_right[4] = sheet2.crop(width*4,0,width, height);
+        player_right[5] = sheet2.crop(width*5,0,width, height);
+
+        player_left = new BufferedImage[6];
+        player_left[0] = sheet2.crop(0, height,width,height);
+        player_left[1] = sheet2.crop(width, height,width,height);
+        player_left[2] = sheet2.crop(width*2, height,width,height);
+        player_left[3] = sheet2.crop(width*3, height,width,height);
+        player_left[4] = sheet2.crop(width*4, height,width,height);
+        player_left[5] = sheet2.crop(width*5, height,width,height);
+
+        player_up = new BufferedImage[6];
+        player_up[0] = sheet2.crop(0, height*2, width, height);
+        player_up[1] = sheet2.crop(width, height*2, width, height);
+        player_up[2] = sheet2.crop(width*2, height*2, width, height);
+        player_up[3] = sheet2.crop(width*3, height*2, width, height);
+        player_up[4] = sheet2.crop(width*4, height*2, width, height);
+        player_up[5] = sheet2.crop(width*5, height*2, width, height);
+
+        player_down = new BufferedImage[6];
+        player_down[0] =sheet2.crop(0,3*height,width,height);
+        player_down[1] =sheet2.crop(width,3*height,width,height);
+        player_down[2] =sheet2.crop(width*2,3*height,width,height);
+        player_down[3] =sheet2.crop(width*3,3*height,width,height);
+        player_down[4] =sheet2.crop(width*4,3*height,width,height);
+        player_down[5] =sheet2.crop(width*5,3*height,width,height);
+
+
+
+        dirt = sheet.crop(0,0,width,height);
+        grass = sheet.crop(width, 0, width, height);
+        water = sheet.crop(2*width, 0, width, height);
+        tree = sheet.crop(3*width,0, width, height);
+        stone =sheet.crop(width, height,width,height);
+        lava = sheet.crop(2*width,height,width,height);
+        brick = sheet.crop(3*width, height, width, height);
 
     }
 }
